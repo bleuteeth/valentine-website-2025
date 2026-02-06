b57e3a646fe2d157bc161b683bffbcf65f9078fc
bleuteeth <sofiavenicereyes22@gmail.com>
2026-02-06 13:24:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dz2nfcemt/video/upload/v1770371264/Clairo_-_Sofia_-_Claire_Cottrill_nq2ohr.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dz2nfcemt/video/upload/v1770370551/IV_OF_SPADES_-_Captivated_Official_Audio_-_IV_OF_SPADES_udbh4l.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
